662aebca174775712547e8f77cb8ca81b30f0027
Hiroshi Yokoyama <h.yokoyama.11323885@gmail.com>
2022-11-24 11:59:24 +0900

    modify visualizations

--- numerical_sim/demo_vbEnKF_stat.py
+++ numerical_sim/demo_vbEnKF_stat.py
@@ -93,22 +93,24 @@ def main():
         gs  = fig.add_gridspec(2,3)
         plt.subplots_adjust(wspace=0.5, hspace=0.6)
         
+        time_range = np.array([[ 5,  6],
+                               [15, 16],
+                               [25, 26]])
         
         ax1 = fig.add_subplot(gs[0, 0:3])
-        ax1.plot(time, eeg, label='exact', zorder=1);
-        ax1.plot(time[1:], eeg_pred[0,:-1],    label='estimated', zorder=2);
+        ax1.plot(time, eeg, c='#1f77b4', label='exact', zorder=1, alpha=0.7, linewidth=1.5);
+        ax1.plot(time[1:], eeg_pred[0,:-1], color='#ff7f0e', label='predicted', zorder=2, alpha=0.7, linewidth=1.5);
+        [ax1.plot(time_range[i,:], [-0.8,-0.8], linewidth=3, c='k') for i in range(3)]
         ax1.set_xlabel('time (s)')
         ax1.set_ylabel('amplitude (a.u.)')
         ax1.set_ylim(-1, 18)
         ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=26, frameon=False)
         ############
-        time_range = np.array([[ 2,  4],
-                               [ 9, 11],
-                               [16, 18]])
+        
         for i in range(3):
             axn = fig.add_subplot(gs[1, i])
-            axn.plot(time, eeg, label='exact', zorder=1);
-            axn.plot(time[:-1], eeg_pred[0,1:],    label='estimated', zorder=2);
+            axn.plot(time, eeg, c='#1f77b4', label='exact', zorder=1, alpha=0.7, linewidth=1.5);
+            axn.plot(time[:-1], eeg_pred[0,1:], color='#ff7f0e', label='estimated', zorder=2, alpha=0.7, linewidth=1.5);
             axn.set_xlabel('time (s)')
             axn.set_xlim(time_range[i,:])
             axn.set_ylim(-1, 18)
@@ -119,6 +121,116 @@ def main():
         plt.savefig(fig_save_dir + 'eeg_Npar%d.svg'%Npar, bbox_inches="tight")
         plt.show()
         
+        #%%
+    
+        
+        fig = plt.figure(figsize=(10, 18))
+        gs  = fig.add_gridspec(6,4,height_ratios=[1.5,0.01,1,1,1, 0.1])
+        plt.subplots_adjust(wspace=0.8, hspace=0.8)
+        ### mEI ratio
+        mEI_true = param_true[:,0]/(param_true[:,0] + param_true[:,2])
+        mEI_pred = param_pred[:,:,0]/(param_pred[:,:,0] + param_pred[:,:,2])
+        
+        ax = fig.add_subplot(gs[0, 0:])
+        ax.plot(time, mEI_true, color='#1f77b4', label='exact', zorder=2, alpha=0.7, linewidth=2.5);
+        ax.plot(time, mEI_pred.T, color='#ff7f0e', label='estimated', zorder=1, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(0.05, 0.4)
+        ax.legend(['exact', 'estimated'],bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=26, frameon=False)
+        ax.set_title('mEI ratio\n$mEI = A(t)/(A(t)+B(t))$')
+        
+        ### parameter A
+        ax = fig.add_subplot(gs[2, 0:2])
+        ax.plot(time, param_true[:,0], c='#1f77b4', label='exact', zorder=2, alpha=0.7, linewidth=2.5);
+        lines = [np.column_stack([time, param_pred[j,:,0]]) for j in range(Ntri)]
+        lc = LineCollection(lines, cmap='autumn', linewidth=1.5)
+        lc.set_array(np.arange(0, Ntri))
+        lc.set_alpha(0.5)
+        line = ax.add_collection(lc) #add to the subplot
+        # ax.plot(time, param_pred[:,:,0].T, c='#ff7f0e', label='estimated', zorder=1, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(2, 7)
+        ax.set_title('$A(t)$')
+        
+        ### parameter B
+        ax = fig.add_subplot(gs[2, 2:])
+        ax.plot(time, param_true[:,2], c='#1f77b4', label='exact', zorder=2, alpha=0.7, linewidth=2.5);
+        lines = [np.column_stack([time, param_pred[j,:,2]]) for j in range(Ntri)]
+        lc = LineCollection(lines, cmap='autumn', linewidth=1.5)
+        lc.set_array(np.arange(0, Ntri))
+        lc.set_alpha(0.5)
+        line = ax.add_collection(lc) #add to the subplot
+        # ax.plot(time, param_pred[:,:,2].T, c='#ff7f0e', label='estimated', zorder=1, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(10, 30)
+        ax.set_title('$B(t)$')
+        
+        
+        ### parameter a
+        ax = fig.add_subplot(gs[3, 0:2])
+        ax.plot(time, param_true[:,1], c='#1f77b4', label='exact', zorder=2, alpha=0.7, linewidth=2.5);
+        lines = [np.column_stack([time, param_pred[j,:,1]]) for j in range(Ntri)]
+        lc = LineCollection(lines, cmap='autumn', linewidth=1.5)
+        lc.set_array(np.arange(0, Ntri))
+        lc.set_alpha(0.5)
+        line = ax.add_collection(lc) #add to the subplot
+        # ax.plot(time, param_pred[:,:,1].T, c='#ff7f0e', label='estimated', zorder=1, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(80, 120)
+        ax.set_title('$a(t)$')
+        
+        ### parameter b
+        ax = fig.add_subplot(gs[3, 2:])
+        ax.plot(time, param_true[:,3], c='#1f77b4', label='exact', zorder=2, alpha=0.7, linewidth=2.5);
+        lines = [np.column_stack([time, param_pred[j,:,3]]) for j in range(Ntri)]
+        lc = LineCollection(lines, cmap='autumn', linewidth=1.5)
+        lc.set_array(np.arange(0, Ntri))
+        lc.set_alpha(0.5)
+        line = ax.add_collection(lc) #add to the subplot
+        # ax.plot(time, param_pred[:,:,3].T, c='#ff7f0e', label='estimated', zorder=1, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(40, 70)
+        ax.set_title('$b(t)$')
+        
+        ### parameter p
+        ax = fig.add_subplot(gs[4, 1:3])
+        ax.plot(time, param_true[:,4], c='#1f77b4', zorder=1, alpha=0.7, linewidth=1.5);
+        lines = [np.column_stack([time, param_pred[j,:,4]]) for j in range(Ntri)]
+        lc = LineCollection(lines, cmap='autumn', linewidth=1.5)
+        lc.set_array(np.arange(0, Ntri))
+        lc.set_alpha(0.5)
+        line = ax.add_collection(lc) #add to the subplot
+        # ax.plot(time, param_pred[:,:,4].T, c='#ff7f0e', zorder=2, alpha=0.7, linewidth=1.5);
+        ax.set_xlabel('time (s)')
+        ax.set_ylabel('amplitude (a.u.)')
+        ax.set_ylim(60, 420)
+        ax.set_title('$p(t)$')
+        ax.legend(['exact'],bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, fontsize=26, frameon=False)
+        
+        # add color bar below main figure panel
+        ax_cb = fig.add_subplot(gs[5, 1:3])
+        divider = make_axes_locatable(ax_cb)
+        cax = divider.new_vertical(size='10%', pad=1.0, pack_start = True)
+        fig.add_axes(cax)
+        fig.colorbar(line, cax = cax, label='Num. of trial',  orientation='horizontal')
+        ax_cb.xaxis.label.set_color('white')        #setting up X-axis label color to white
+        ax_cb.yaxis.label.set_color('white')        #setting up Y-axis label color to white
+        ax_cb.tick_params(axis='x', colors='white') #setting up X-axis tick color to white
+        ax_cb.tick_params(axis='y', colors='white') #setting up Y-axis tick color to white
+        ax_cb.spines['bottom'].set_color('white')
+        ax_cb.spines['top'].set_color('white')
+        ax_cb.spines['left'].set_color('white')
+        ax_cb.spines['right'].set_color('white')
+        #######
+        plt.savefig(fig_save_dir + 'EIparam_Npar%d.png'%Npar, bbox_inches="tight")
+        plt.savefig(fig_save_dir + 'EIparam_Npar%d.svg'%Npar, bbox_inches="tight")
+        plt.show()
+        #%%
         cnt += 1
     #%%
     fig_save_dir = current_path + '/figures/'
@@ -129,8 +241,7 @@ def main():
     for idx in tick_idx:
         xtick_lab.append(str(Npar_list[idx]))
     
-    # plt.scatter(Npar_list, MAE.mean(axis=0))
-    sns.violinplot(data=MAE, color='skyblue')
+    plt.violinplot(MAE, positions=np.arange(0,len(Npar_list)), showextrema=True, showmedians=True)
     plt.xticks(ticks=tick_idx, labels = xtick_lab)
     plt.xlabel('Num. of ensemble')
     plt.ylabel('MAE (a.u.)')
@@ -138,9 +249,8 @@ def main():
     plt.savefig(fig_save_dir + 'MAE.svg'%Npar, bbox_inches="tight")
     plt.show()
     
-    # plt.scatter(Npar_list, R_est_all.mean(axis=0))
-    sns.violinplot(data=R_est_all, color='skyblue')
-    plt.plot([0, len(Npar_list)], [1.3, 1.3], 'r--', linewidth=2, zorder=0)
+    plt.violinplot(R_est_all, positions=np.arange(0,len(Npar_list)), showextrema=True, showmedians=True)
+    plt.plot([0, len(Npar_list)], [1.3, 1.3], 'r--', linewidth=2, zorder=0, alpha=0.7)
     plt.xticks(ticks=tick_idx, labels = xtick_lab)
     plt.xlabel('Num. of ensemble')
     # plt.yticks(ticks=np.arange(1, 8, 2))
